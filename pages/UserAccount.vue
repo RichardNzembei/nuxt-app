12e76eb9aeda6681ab4da9e032625ba2d96bdfa2
RichardNzembei <richardsonreuben78@gmail.com>
2024-11-12 20:22:05 +0300

    creating a chip with nuxt ui

--- pages/UserAccount.vue
+++ pages/UserAccount.vue
@@ -1,3 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+const count=ref(3)
+</script>
 <template>
-    <h2>user</h2>
-</template>
+    <div class="flex justify-center items-center mt-5">
+        <UChip :text="count"  size="2xl">
+      <UButton icon="i-heroicons-inbox" color="yellow" />
+    </UChip>
+    </div>
+ 
+  </template>
+  
